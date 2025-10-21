a61c98720cc6334fcda30ae3b072193e3ca2f609
gamelist1990 <testbing@outlook.jp>
2025-10-22 08:59:02 +0900

    プロジェクトの初期設定を追加
    - .gitignoreファイルを作成し、ユーザー固有のファイルやビルド成果物を無視する設定を追加
    - build.gradleファイルを作成し、必要なプラグインと依存関係を設定
    - gradle.propertiesファイルを作成
    - gradle-wrapper.propertiesファイルを作成し、Gradleのバージョンを指定
    - settings.gradleファイルを作成し、ルートプロジェクト名を設定
    - Pexsurvivalクラスを含むJavaプラグインのメインファイルを作成
    - plugin.ymlファイルを作成し、プラグインのメタデータを設定

---
+++ src/main/java/org/pexserver/koukunn/pexsurvival/Pexsurvival.java
@@ -0,0 +1,17 @@
+package org.pexserver.koukunn.pexsurvival;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Pexsurvival extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
